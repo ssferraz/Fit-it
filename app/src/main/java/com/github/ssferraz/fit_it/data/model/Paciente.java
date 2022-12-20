e899776d5bef0560d25a98eca15202dd1caf9f57
cvandeley <cvandeley@ness.com.br>
2022-12-20 16:46:34 -0300

    feature: autenticacao

--- app/src/main/java/com/github/ssferraz/fit_it/data/model/Paciente.java
+++ app/src/main/java/com/github/ssferraz/fit_it/data/model/Paciente.java
@@ -1,4 +1,58 @@
 package com.github.ssferraz.fit_it.data.model;
 
 public class Paciente {
+    private int Id;
+    private String Nome;
+    private String Cidade;
+    private String Telefone;
+    private String Email;
+    private String Senha;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getCidade() {
+        return Cidade;
+    }
+
+    public void setCidade(String cidade) {
+        Cidade = cidade;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        Telefone = telefone;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getSenha() {
+        return Senha;
+    }
+
+    public void setSenha(String senha) {
+        Senha = senha;
+    }
 }
